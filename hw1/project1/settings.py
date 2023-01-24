88e3e42a8ea4c91addb3f01f55eebdeac366c6ea
adolfxuhj <adolfxuhj@163.com>
2023-01-24 17:02:42 -0500

    init

--- hw1/project1/settings.py
+++ hw1/project1/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #add my new applications
-    'testapp.apps'
-    
+    #add my new application
+    'basicapp'
 ]
 
 MIDDLEWARE = [
