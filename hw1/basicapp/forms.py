51e814917bfde5f1751ba39b37bce44e92bf796f
mutian <mutian.ling@outlook.com>
2023-01-25 16:46:20 -0500

    register done

--- hw1/basicapp/forms.py
+++ hw1/basicapp/forms.py
@@ -7,6 +7,10 @@ class UserRegisterForm(forms.ModelForm):
 
     class Meta():
         model = Users
-        fields = ('','','')
-        
-        
+        fields = ('user_id','user_pwd')
+        widgets ={'user_id': forms.EmailInput(),
+                    'user_pwd':forms.PasswordInput(),}
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['user_id'].label = "Email"
+        self.fields['user_pwd'].label = "Password"    
--- hw1/project1/settings.py
+++ hw1/project1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #add my new applications
     'basicapp',
+    'bootstrap4',
     
     
 ]
