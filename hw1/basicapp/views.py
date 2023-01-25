d9a6e0165464cc0962ae7591e1c8aeb3a40d50cd
mutian <mutian.ling@outlook.com>
2023-01-25 13:04:38 -0500

    homepage

--- hw1/basicapp/views.py
+++ hw1/basicapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import (TemplateView,ListView,DeleteView,
                                     CreateView,UpdateView,DetailView,RedirectView
                                     )
+
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = "base.html"
+    template_name = "homepage.html"
+
