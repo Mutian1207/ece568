faf368f2a379efefaf7afc3a655d28e39c2f0576
adolfxuhj <adolfxuhj@163.com>
2023-01-25 15:14:23 -0500

    models v1

--- hw1/basicapp/models.py
+++ hw1/basicapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    VEHICLE_TYPE = (
+        ('su','suv'),
+        ('mp','mpv'),
+        ('sd','sedan'),
+        )
+    user_id = models.EmailField(primary_key=True)
+    user_pwd = models.CharField(max_length=30)
+    is_driver = models.BooleanField()
+    vehic_type = models.CharField(max_length=2, null=True, choices=VEHICLE_TYPE)
+    lice_plate_number = models.CharField(max_length=10, null=True)
+    max_pass_num = models.PositiveSmallIntegerField(null=True)
+    other_reg = models.TextField(max_length=300, null=True)
+
+class Rides(models.Model):
+    STATUS = (
+        ('op','open'),
+        ('cf','confirmed'),
+        ('cp','complete'),
+        )
+    ride_id = models.BigAutoField(primary_key=True)
+    owner = models.ForeignKey('Users', on_delete=models.CASCADE, related_name='owners')
+    dest_addr = models.CharField(max_length=200)
+    arr_date_time = models.DateTimeField()
+    party_num = models.PositiveSmallIntegerField()
+    sharable = models.BooleanField()
+    status = models.CharField(max_length=2, choices=STATUS)
+    driver_acc = models.ForeignKey('Users', on_delete=models.CASCADE, related_name='drivers')
+    other_reg = models.TextField(max_length=300, null=True)
+
+class Sharers(models.Model):
+    share_id = models.ForeignKey('Rides', on_delete=models.CASCADE, related_name='share_ride')
+    sharer_id = models.ForeignKey('Users', on_delete=models.CASCADE, related_name='sharers')
+    share_party_num = models.PositiveSmallIntegerField()
