d5c680fddbee53ff966d692c8b43000e314bc225
adolfxuhj <adolfxuhj@163.com>
2023-01-28 13:51:18 -0500

    new

--- hw1/basicapp/urls.py
+++ hw1/basicapp/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('logout/',views.UserLogoutView.as_view(),name = "logout"),
     path('success/',views.OrderSuccessView.as_view(),name = "ordersuccess"),
     re_path(r'^/(?P<pk>\d+)/$',views.PersonalInfoView.as_view(),name = "personalinfo"),
-    path('rides/<int:pk>/',views.UserRides.as_view(),name = "rides"),
-
+    re_path(r'^rides/(?P<pk>\d+)/$',views.UserRidesView.as_view(),name = "rides"),
 ]
